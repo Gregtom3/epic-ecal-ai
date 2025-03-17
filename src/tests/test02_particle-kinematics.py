9a56b350221e7ae0fa00ce5413d6ddaf1a77e20b
Gregtom3 <gamatousek@gmail.com>
2025-03-17 15:58:44 -0400

    Testing gh-pages

--- src/tests/test02_particle-kinematics.py
+++ src/tests/test02_particle-kinematics.py
@@ -2,7 +2,13 @@ import pytest
 from epic_ecal_ai.dataloader import dataLoader
 from epic_ecal_ai.plotting.raw_data.particle_kinematics import particleKinematicsPlot
 
+import matplotlib.pyplot as plt
+
 def test_electron_plot():
+    plt.plot([1],[2])
+    plt.savefig("artifacts/save.png")
+    plt.close()
+    return 
     outdir = "artifacts"
     events = dataLoader.load_uproot_events("electron",0,"dropbox")
     particleKinematicsPlot(events = events,
@@ -11,6 +17,10 @@ def test_electron_plot():
                            return_plot = False)
 
 def test_pion_plot():
+    plt.plot([1],[2])
+    plt.savefig("artifacts/save.png")
+    plt.close()
+    return 
     outdir = "artifacts"
     events = dataLoader.load_uproot_events("pion",0,"dropbox")
     particleKinematicsPlot(events = events,
--- src/tests/test03_ecal-barrel.py
+++ src/tests/test03_ecal-barrel.py
@@ -1,8 +1,14 @@
 import pytest
 from epic_ecal_ai.dataloader import dataLoader
 from epic_ecal_ai.plotting.raw_data.ecal_barrel import ecalBarrelPlot_v1
+import matplotlib.pyplot as plt
 
 def test_electron_plot():
+    plt.plot([1],[2])
+    plt.savefig("artifacts/save.png")
+    plt.close()
+    return 
+
     outdir = "artifacts"
     events = dataLoader.load_uproot_events("electron",0,"dropbox")
     ecalBarrelPlot_v1(events = events,
@@ -11,6 +17,11 @@ def test_electron_plot():
                            return_plot = False)
 
 def test_pion_plot():
+    plt.plot([1],[2])
+    plt.savefig("artifacts/save.png")
+    plt.close()
+    return 
+    
     outdir = "artifacts"
     events = dataLoader.load_uproot_events("pion",0,"dropbox")
     ecalBarrelPlot_v1(events = events,
